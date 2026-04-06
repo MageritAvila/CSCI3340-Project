2a07823d3ea5aca8c5cb0b7df70744b6065907b7
John Reyes <reyes.john1005@gmail.com>
2026-04-06 15:06:53 -0500

    minor changes to simplify database display

--- Test/projectManager/admin.py
+++ Test/projectManager/admin.py
@@ -1,9 +1,18 @@
 from django.contrib import admin
+from .models import User, Project, Task, Comment
 
-from .models import Task, Project, User, Comment
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['username', 'email', 'UID']
 
-# Register your models here.
-admin.site.register(Task)
-admin.site.register(Project)
-admin.site.register(User)
-admin.site.register(Comment)
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ['name', 'creator']
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ['name', 'creator', 'due_date']
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['user', 'project', 'task', 'timestamp']
