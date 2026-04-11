1154ec9deeb63771e1313798cade63f94b4f8505
MageritAvila <magerit.avila01@utrgv.edu>
2026-04-11 01:38:24 +0000

    commit2

--- Test/Test/urls.py
+++ Test/Test/urls.py
@@ -20,4 +20,5 @@ from django.urls import include, path
 urlpatterns = [
     path("projectManager/", include("projectManager.urls")),
     path("admin/", admin.site.urls),
+    path('', include('projectManager.urls')),
 ]
