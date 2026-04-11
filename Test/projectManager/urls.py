2193dcd1f60cb21c7ceb873285d2dee57d60439e
MageritAvila <magerit.avila01@utrgv.edu>
2026-04-11 00:52:17 +0000

    home_working1

--- Test/projectManager/urls.py
+++ Test/projectManager/urls.py
@@ -3,6 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("dashboard/<str:user_id>/", views.dashboard, name="dashboard"),
+    path('', views.home, name ='home'),
 ]
+
+# urlpatterns = [
+#    path("", views.index, name="index"),
+#    path("dashboard/<str:user_id>/", views.dashboard, name="dashboard"),
+#]
--- Test/projectManager/views.py
+++ Test/projectManager/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
+
+def home(request):
+	return render(request, 'home.html',{})
 
 
-def index(request):
-    return HttpResponse("This will be the Project Manager index. Can be login/signup page with link to dashboard.")
+# def index(request):
+#    return HttpResponse("This will be the Project Manager index. Can be login/signup page with link to dashboard.")
 
-def dashboard(request):
-    return HttpResponse("This will be the Project Manager dashboard, showing projects and tasks.")
+#def dashboard(request):
+#    return HttpResponse("This will be the Project Manager dashboard, showing projects and tasks.")
